5e8a75f958680ac8fb675093d884d5203cc26d5d
MHDMufeek <mhdmufeek@gmail.com>
2025-08-21 16:28:36 +0530

    First commit - profile project

---
+++ HTML/script.js
@@ -0,0 +1,13 @@
+//wellcome massege
+alert("Welcome to Mufeek's Profile");
+
+//log details
+let name = "Mufeek";
+let skills = ["HTML","CSS","PHP","React"];
+console.log("Name:,name");
+console.log("Skills:",skills);
+
+//Button action
+function showMessage() {
+    alert("Thank for visiting my profile!")
+}
